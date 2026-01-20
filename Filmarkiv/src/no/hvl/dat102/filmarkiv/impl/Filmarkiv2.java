0ee38460919fd63fedd5938babd70599bf7685be
190331-N <190331@hvl.no>
2026-01-20 21:36:11 +0100

    j

---
+++ Filmarkiv/src/no/hvl/dat102/filmarkiv/impl/Filmarkiv2.java
@@ -0,0 +1,10 @@
+package no.hvl.dat102.filmarkiv.impl;
+
+import no.hvl.dat102.filmarkiv.adt.FilmarkivADT;
+
+public class Filmarkiv2 implements FilmarkivADT {
+
+    private int antall;
+    private LinearNode<Film> start;
+
+}
