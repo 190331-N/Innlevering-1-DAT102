8bbd9f402072b90aa86cc26e6a44b9fdc9b4c1e5
HansArne04 <hansarnemid@gmail.com>
2026-01-22 10:18:50 +0100

    a

--- Filmarkiv/src/no/hvl/dat102/filmarkiv/klient/Meny.java
+++ Filmarkiv/src/no/hvl/dat102/filmarkiv/klient/Meny.java
@@ -1,6 +1,8 @@
 package no.hvl.dat102.filmarkiv.klient;
 
 import no.hvl.dat102.filmarkiv.adt.FilmarkivADT;
+import no.hvl.dat102.filmarkiv.impl.Film;
+import no.hvl.dat102.filmarkiv.impl.Sjanger;
 
 
 public class Meny {
@@ -13,8 +15,12 @@ public class Meny {
         this.filmarkiv = filmarkiv;
     }
     public void start(){
-// legg inn en del forhåndsdefinerte filmer for å teste metodene
-// ..
+        Film f1 = new Film(1, "Peter Jackson", "Ringenes herre", 2001, Sjanger.FANTASY, "New Line");
+        Film f2 = new Film(2, "James Cameron", "Titanic", 1997, Sjanger.DRAMA, "Fox");
+        Film f3 = new Film(3, "Gore Verbinski", "Pirates", 2003, Sjanger.EVENTYR, "Disney");
+
+
+
 // TODO
     }
 }
